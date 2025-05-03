e76b241b58fde3d13d8ea1b762247924536ed8a7
wwqby <294111927@qq.com>
2025-05-03 19:33:57 +0800

    添加生成动画和逻辑

--- Assets/Scipt/EnemyMovement.cs
+++ Assets/Scipt/EnemyMovement.cs
@@ -5,45 +5,76 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField]
-    private Player player;
-    [SerializeField]
+    [Header("Spawn Related")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private SpriteRenderer spawnerIndicator;
+    [SerializeField] private float scaleTime;
+    [SerializeField] private Player player;
     [Header("Movement")]
-    private float moveSpeed = 0;
-    [SerializeField]
-    private float playerDetectionRadius;
+    [SerializeField] private float moveSpeed = 0;
+    [SerializeField] private float playerDetectionRadius;
     [Header("Effects")]
-    [SerializeField]
-    private ParticleSystem particle;
+    [SerializeField] private ParticleSystem particle;
+    private bool hasSpawned;
+
     // Start is called before the first frame update
     void Start()
     {
-        player  = FindAnyObjectByType<Player>();
-        if (player == null){
+        player = FindAnyObjectByType<Player>();
+        if (player == null)
+        {
             Debug.LogWarning("Player not found!");
             Destroy(gameObject);
         }
+        //hide the enemy
+        spriteRenderer.enabled = false;
+        //show the indicator
+        spawnerIndicator.enabled = true;
+
+        //scale up and down the indicator
+
+        Vector3 targetScale = spawnerIndicator.transform.localScale * 1.2f;
+        LeanTween.scale(spawnerIndicator.gameObject, targetScale, scaleTime)
+        .setLoopPingPong(4)
+        .setOnComplete(SpawnCompleted);
+        //prevent the enemy follow and attack during the update()
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        if(!hasSpawned){
+            return;
+        }
         FollowPlayer();
         TryAttack();
     }
 
+    private void SpawnCompleted()
+    {
+        //show the enemy
+        spriteRenderer.enabled = true;
+        //hide the indicator
+        spawnerIndicator.enabled = false;
+        hasSpawned = true;
+    }
+
+
+
     private void TryAttack()
     {
         float distance = Vector2.Distance(player.transform.position, transform.position);
-        if (distance < playerDetectionRadius){
+        if (distance < playerDetectionRadius)
+        {
             PassAway();
         }
     }
 
     private void PassAway()
     {
-        if(particle!=null){
-            particle.transform.parent = null ;
+        if (particle != null)
+        {
+            particle.transform.parent = null;
             particle.Play();
         }
         Destroy(gameObject);
