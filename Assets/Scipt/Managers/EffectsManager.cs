92a31aff9c2803ac34dfd3b2635a0ab74b2c3a08
wwqby <294111927@qq.com>
2025-05-07 20:46:49 +0800

    为特效对象添加资源池

--- Assets/Scipt/Managers/EffectsManager.cs
+++ Assets/Scipt/Managers/EffectsManager.cs
@@ -1,27 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class EffectsManager : MonoBehaviour
 {
     [Header("components")]
     [SerializeField] private DamageText DamageTextPrefab;
-    // Start is called before the first frame update
+    [Header("Pooling")]
+    [SerializeField] private ObjectPool<DamageText> DamageTextPool;
 
     void OnEnable()
     {
-        Enemy.OnTakeDamage += InitializeEffects;
+        Enemy.OnTakeDamage += ShowDamageTextOnPosition;
     }
 
     void OnDisable()
     {
-        Enemy.OnTakeDamage -= InitializeEffects;
+        Enemy.OnTakeDamage -= ShowDamageTextOnPosition;
     }
 
-    [NaughtyAttributes.Button]
-    private void InitializeEffects(int damage, Vector2 position)
+    void Start()
     {
-        DamageText instance = Instantiate<DamageText>(DamageTextPrefab, position + Random.insideUnitCircle * 2f, Quaternion.identity, transform);
+        DamageTextPool = new ObjectPool<DamageText>(CreateFucc,ActionOnGet,ActionOnRelease,ActionOnDestory);
+    }
+
+    #region Pooling
+    private void ActionOnDestory(DamageText damageText)
+    {
+        Destroy(damageText.gameObject);
+    }
+
+    private void ActionOnRelease(DamageText damageText)
+    {
+         damageText.gameObject.SetActive(false);
+    }
+
+    private void ActionOnGet(DamageText damageText)
+    {
+        damageText.gameObject.SetActive(true);
+    }
+
+    private DamageText CreateFucc()
+    {
+        return Instantiate<DamageText>(DamageTextPrefab,transform);
+    }
+
+    #endregion
+
+    private void ShowDamageTextOnPosition(int damage, Vector2 position)
+    {
+        DamageText instance = DamageTextPool.Get();
+        instance.transform.position = position + UnityEngine.Random.insideUnitCircle * 2f;
         instance.ShowDamageText(damage);
+        LeanTween.delayedCall(1.2f,()=>{
+            DamageTextPool.Release(instance);
+        });
     }
 }
